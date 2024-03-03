913d7d68dbd4bb65cccbd18b9af556c528e4279d
Lucas <lucass_silvas@hotmail.com>
2024-03-03 20:34:39 -0300

    add JSDoc description to function

--- src/extras.ts
+++ src/extras.ts
@@ -1,3 +1,8 @@
+/**
+ * Attempts to extract URLs from `data`
+ * @param data The `data` property from `NotificationInfo.NotificationData`
+ * @returns
+ */
 export function getUrlsFromData(data: string) {
   const regex =
     /(http|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])/g;

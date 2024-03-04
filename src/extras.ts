e82a2524ce044def51344f92b95ff843c64b209a
Lucas <lucass_silvas@hotmail.com>
2024-03-04 15:33:58 -0300

    fix getUrlsFromData to return string instead of RegExpArray

--- src/extras.ts
+++ src/extras.ts
@@ -1,10 +1,9 @@
 /**
  * Attempts to extract URLs from `data`
  * @param data The `data` property from `NotificationInfo.NotificationData`
- * @returns
  */
 export function getUrlsFromData(data: string) {
   const regex =
-    /(http|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])/g;
-  return data.match(regex);
+    /(?<url>(http|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-]))/;
+  return data.match(regex)?.groups?.url;
 }
